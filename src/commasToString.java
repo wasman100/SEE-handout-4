4e11e96921ab16ec785724a0e429f702200ce34f
wesley sharpe <wesmon50@yahoo.com>
2020-09-18 21:07:46 -0400

    started and got the commas to string working

---
+++ src/commasToString.java
@@ -0,0 +1,43 @@
+import acm.program.*;
+
+public class commasToString extends ConsoleProgram {
+	public void run() {
+		 while (true) {
+		 String digits = readLine("Enter a numeric string: ");
+		 if (digits.length() == 0) break;
+		 println(addCommasToNumericString(digits));
+		 }
+		}
+
+	private String addCommasToNumericString(String digits) {
+		// TODO Auto-generated method stub
+		String placeholder="";
+		int letterSelector = digits.length();
+		try {
+			// making sure it is a real number 
+			int testing = Integer.parseInt(digits);
+			//looping through to make sure at every 3rd char we add a comma 
+			for(int i=0; i<digits.length(); i++) {
+				letterSelector--;
+				if(letterSelector < 0) {
+					letterSelector = 0;
+				}
+				//println(letterSelector);
+				if(i%3 == 0 && i != 0) {
+					placeholder += ",";
+				}
+				placeholder += digits.charAt(letterSelector);
+			}
+			//reversing the string so we get the right number instead of it backwards
+			return( new StringBuilder(placeholder).reverse().toString());
+			
+			
+			}
+			catch (NumberFormatException e)
+			{
+				return null;
+			}
+
+	}
+
+}
